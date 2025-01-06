4c92717f75bfc25ed09cbf837c261507d0ed3990
aibekzhumabekov <aibek.zhumabekov@proton.me>
2025-01-06 00:10:38 -0500

    Enhance EmployeeClass to demonstrate full usage of all methods
    - Added usage of setName and setAge methods to update employee details
    - Display initial and updated values using getters and toString()
    - Ensured all methods in the Employee class are utilized for completeness

--- src/EmployeeClass.java
+++ src/EmployeeClass.java
@@ -1,11 +1,26 @@
 public class EmployeeClass {
 
     public static void main(String[] args) {
+        // Create Employee objects using the constructor
         Employee me = new Employee("Troy", 35);
         Employee you = new Employee("Dan", 60);
 
+        // Use getters to display initial values
+        System.out.println("Initial values:");
         System.out.println(me);
         System.out.println(you);
+
+        // Modify values using setters
+        me.setName("Troy Johnson");
+        me.setAge(36);
+
+        you.setName("Daniel Smith");
+        you.setAge(61);
+
+        // Use getters to display updated values
+        System.out.println("\nUpdated values:");
+        System.out.println("Name: " + me.getName() + ", Age: " + me.getAge());
+        System.out.println("Name: " + you.getName() + ", Age: " + you.getAge());
     }
 }
 
